eaf6365a21b965271bb4acac71f89f69dcbd9ba7
Calvin Remsburg <cremsburg@protonmail.com>
2021-07-04 19:19:42 +0000

    init

--- files/config/containerhost.sh
+++ files/config/containerhost.sh
@@ -39,7 +39,6 @@ docker exec -it clab-mpls001-pe4 ip addr add 10.2.14.14/24 dev eth3
 docker exec -it clab-mpls001-pe4 ip addr add 192.168.255.14/32 dev lo
 
 # p[1-2]
-docker exec -it clab-mpls001-p1 ip addr add 192.168.105.141/24 dev eth1
 docker exec -it clab-mpls001-p1 ip addr add 10.1.11.1/24 dev eth2
 docker exec -it clab-mpls001-p1 ip addr add 10.1.12.1/24 dev eth3
 docker exec -it clab-mpls001-p1 ip addr add 10.1.13.1/24 dev eth4
@@ -48,15 +47,17 @@ docker exec -it clab-mpls001-p1 ip addr add 10.1.2.1/24 dev eth6
 docker exec -it clab-mpls001-p1 ip addr add 10.1.4.1/24 dev eth7
 docker exec -it clab-mpls001-p1 ip addr add 192.168.255.1/32 dev lo
 
-docker exec -it clab-mpls001-p2 ip addr add 192.168.105.142/24 dev eth1
 docker exec -it clab-mpls001-p2 ip addr add 10.2.11.2/24 dev eth2
 docker exec -it clab-mpls001-p2 ip addr add 10.2.12.2/24 dev eth3
 docker exec -it clab-mpls001-p2 ip addr add 10.2.13.2/24 dev eth4
 docker exec -it clab-mpls001-p2 ip addr add 10.2.14.2/24 dev eth5
 docker exec -it clab-mpls001-p2 ip addr add 10.1.2.2/24 dev eth6
-docker exec -it clab-mpls001-p1 ip addr add 10.1.5.2/24 dev eth7
+docker exec -it clab-mpls001-p2 ip addr add 10.2.5.2/24 dev eth7
 docker exec -it clab-mpls001-p2 ip addr add 192.168.255.2/32 dev lo
 
 # rr[1-2]
 docker exec -it clab-mpls001-rr1 ip addr add 10.1.4.4/24 dev eth1
+docker exec -it clab-mpls001-rr1 ip addr add 192.168.255.21/32 dev lo
+
 docker exec -it clab-mpls001-rr2 ip addr add 10.2.5.5/24 dev eth1
+docker exec -it clab-mpls001-rr2 ip addr add 192.168.255.21/32 dev lo
